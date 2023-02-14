32f44a476feea2a43083828e2e087a5c24866223
Alex Akama <gremcox@bk.ru>
2023-02-14 12:31:55 +0500

    Added day 1 task 02

---
+++ src/main/java/traning_3/division_b/day1/task_02/Task_1_2.java
@@ -0,0 +1,36 @@
+package traning_3.division_b.day1.task_02;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Task_1_2 {
+
+    public static void main(String[] args) throws IOException {
+        var input = "src/main/java/day1/task_2/input.txt";
+        File file = new File(input);
+        FileReader fileReader = new FileReader(file);
+        try (BufferedReader reader = new BufferedReader(fileReader)) {
+            String line = reader.readLine();
+            int k = Integer.parseInt(line);
+            line = reader.readLine();
+            char[] chars = line.toCharArray();
+            int[] w = new int[26];
+            int max = 0;
+            int p = 0;
+            for (int i = 0; i < chars.length; i++) {
+                max = Math.max(max, ++w[chars[i] - 'a']);
+                if (max + k - 1 < i - p) w[chars[p++] - 'a']--;
+            }
+            String res = String.valueOf(chars.length - p);
+            try (FileWriter writer = new FileWriter("src/main/java/day1/task_2/output.txt")) {
+                writer.write(res);
+                writer.flush();
+            }
+        }
+
+    }
+
+}
