cd8f97ebd615cde9febc7d124034d0af07f5ab30
Alex Akama <gremcox@bk.ru>
2023-02-17 15:12:22 +0500

    Added task 14

---
+++ src/main/java/training_3B/task_14/Task_14.java
@@ -0,0 +1,47 @@
+package training_3B.task_14;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Task_14 {
+
+    public static void main(String[] args) throws IOException {
+        var input = "src/main/java/training_3B/task_14/input_3.txt";
+        File file = new File(input);
+        FileReader fileReader = new FileReader(file);
+        try (BufferedReader reader = new BufferedReader(fileReader)) {
+            String line = reader.readLine();
+            int n = Integer.parseInt(line);
+            int[] nums = new int[n];
+            line = reader.readLine();
+            String[] split = line.split(" ");
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(split[i]);
+            }
+
+            System.out.println((isGoodTrain(nums)) ? "YES" : "NO");
+        }
+    }
+
+    private static boolean isGoodTrain(int[] nums) {
+        if (nums.length == 1) return true;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int wait = 1;
+        for (int num : nums) {
+            if (num != wait) stack.add(num);
+            else {
+                wait++;
+                while (!stack.isEmpty() && stack.peekLast() == wait) {
+                    stack.pollLast();
+                    wait++;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
